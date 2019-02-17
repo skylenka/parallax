4a116cba073e4d6fd38f846f36b8b2c61c6d8cc6
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2019-02-17 16:48:27 +0100

    fixed nav and scroll

--- js/init.js
+++ js/init.js
@@ -1,8 +1,14 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener("DOMContentLoaded", function() {
+  var options = {
+    scrollOffset: 50
+  };
+  var elems = document.querySelectorAll(".scrollspy");
+  var instances = M.ScrollSpy.init(elems, options);
+});
